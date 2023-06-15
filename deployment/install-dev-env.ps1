eac756a722f297d87a1d09d0095cb0f56ebce679
Igor Mileusnic <igor.mileusnic@adacta-fintech.com>
2023-06-15 14:32:08 +0200

    updated deploy/upgrade details.

--- deployment/install-dev-env.ps1
+++ deployment/install-dev-env.ps1
@@ -1,7 +1,12 @@
 # https://microsoft.github.io/Mastering-the-Marketplace/saas-accelerator/#setting-up-a-development-environment-for-the-saas-accelerator
 # https://go.microsoft.com/fwlink/?linkid=2224222
 
+###### FIRST DEPLOY COMMAND
 
+wget https://dotnet.microsoft.com/download/dotnet/scripts/v1/dotnet-install.sh; `
+chmod +x dotnet-install.sh; `
+./dotnet-install.sh; `
+$ENV:PATH="$HOME/.dotnet:$ENV:PATH"; `
 dotnet tool install --global dotnet-ef; `
 git clone https://github.com/adactafintech/saas-accelerator.git -b adacta-setup-01 --depth 1; `
 cd ./saas-accelerator/deployment; `
@@ -11,24 +16,36 @@ cd ./saas-accelerator/deployment; `
  -PublisherAdminUsers "igor.mileusnic@adacta-fintech.com" `
  -Location "West Europe" 
 
+###### UPGRADE COMMAND
 
-#  ➡️ Landing Page section:       https://adi-dev-sa-portal.azurewebsites.net/
-#  ➡️ Connection Webhook section: https://adi-dev-sa-portal.azurewebsites.net/api/AzureWebhook
-#  ➡️ Tenant ID:                  cdeb9156-219a-49e3-b414-f63acd298e9c
-#  ➡️ AAD Application ID section: 11b97a06-846d-45eb-83d2-121e54027a13
-
-
-
+wget https://dotnet.microsoft.com/download/dotnet/scripts/v1/dotnet-install.sh; `
+chmod +x dotnet-install.sh; `
+./dotnet-install.sh; `
+$ENV:PATH="$HOME/.dotnet:$ENV:PATH"; `
+dotnet tool install --global dotnet-ef; `
 git clone https://github.com/adactafintech/saas-accelerator.git -b adacta-setup-01 --depth 1; `
 cd ./saas-accelerator/deployment; `
 .\Upgrade.ps1 `
  -WebAppNamePrefix "adi-dev-sa" `
  -ResourceGroupForDeployment "rg-saas-accelerator-dev"
 
+###### DEV PARAMETERS
+
+#  ➡️ Landing Page section:       https://adi-dev-sa-portal.azurewebsites.net/
+#  ➡️ Connection Webhook section: https://adi-dev-sa-portal.azurewebsites.net/api/AzureWebhook
+#  ➡️ Tenant ID:                  cdeb9156-219a-49e3-b414-f63acd298e9c
+#  ➡️ AAD Application ID section: 11b97a06-846d-45eb-83d2-121e54027a13
+
+###### PROD PARAMETERS
+
+# ➡️ Landing Page section:       https://adi-sa-portal.azurewebsites.net/
+# ➡️ Connection Webhook section: https://adi-sa-portal.azurewebsites.net/api/AzureWebhook
+# ➡️ Tenant ID:                  cdeb9156-219a-49e3-b414-f63acd298e9c
+# ➡️ AAD Application ID section: 254acb11-7727-4db4-a58c-1a4fee74928d
 
  # Allowlist IP 89.216.208.132 on server adi-dev-sa-sql
 
-
+###### DEV CONFIGURATION
 
 #  {
 #     "name": "SaaSApiConfiguration__ProvisionAPIBaseURL",
@@ -50,3 +67,28 @@ cd ./saas-accelerator/deployment; `
 #     "value": "ac64baa3ad9884ahe493dfc6kb9077",
 #     "slotSetting": false
 #   },
+
+
+{
+    "name": "SaaSApiConfiguration__ProvisionAPIBaseUrl",
+    "value": "https://git.adacta-fintech.com/api/v4/projects/668/trigger/pipeline",
+    "slotSetting": false
+  },
+  {
+    "name": "SaaSApiConfiguration__ProvisionBranch",
+    "value": "saas-env/release",
+    "slotSetting": false
+  },
+  {
+    "name": "SaaSApiConfiguration__ProvisionToken",
+    "value": "9c25baa49d9884fbe493dfc41b90ee",
+    "slotSetting": false
+  },
+  {
+    "name": "SaaSApiConfiguration__ProvisionWebHookToken",
+    "value": "ac64baa3ad9884ahe493dfc6kb9077",
+    "slotSetting": false
+  },
+
+
+###### PROD CONFIGURATION
